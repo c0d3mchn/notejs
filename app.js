24b142dfa7a0de1fdeb825be3882b5c7d52eec0f
Ilija Damjanovic <c0d3mchn@gmail.com>
2019-11-18 01:16:10 +0100

    Login & logout functionality done

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // Load routes
 const notes = require('./routes/notes');
 const users = require('./routes/users');
+
+// Passport config
+require('./config/passport')(passport);
 
 // MongoDB Init
 mongoose.connect('mongodb://localhost/notejs', {
@@ -28,6 +33,9 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Method override middleware
 app.use(methodOverride('_method'));
 
@@ -38,6 +46,10 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect flash middleware
 app.use(flash());
 
@@ -46,6 +58,7 @@ app.use(function(req, res, next){
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
